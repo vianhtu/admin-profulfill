f9a29c6a7ca27659cbdaa6ca187f8d6b577a8cd3
FOX <thienduongvangem.ntt@gmail.com>
2025-08-12 14:43:58 +0700

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/admin-profulfill',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '205e330205fb8dd254c62be2bb33d1367d203924',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/admin-profulfill' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '205e330205fb8dd254c62be2bb33d1367d203924',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
