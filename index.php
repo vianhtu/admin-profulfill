fcb6ebf45566dac0813b5705ce57ab00e6939036
FOX <thienduongvangem.ntt@gmail.com>
2025-08-13 15:28:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 require __DIR__ . '/config.php';
 
-if (is_logged_in()) {
+if (is_logged_in() || attempt_cookie_login()) {
 	header('Location: ./dashboards.php', true, 302);
 	exit;
 }
