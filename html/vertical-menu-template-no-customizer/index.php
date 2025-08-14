deab08a56444208a585b96a189e52153b4986969
FOX <thienduongvangem.ntt@gmail.com>
2025-08-14 09:53:18 +0700

    ok

--- html/vertical-menu-template-no-customizer/index.php
+++ html/vertical-menu-template-no-customizer/index.php
@@ -209,17 +209,13 @@ $currentMenu = $_GET['menu'] ?? '';
           <div class="content-wrapper">
             <!-- Content -->
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="py-4 mb-6">Page 1</h4>
-              <p>
-                Sample page.<br />For more layout options use
-
-                <a
-                  href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation//layouts.html"
-                  target="_blank"
-                  class="fw-medium"
-                  >Layout docs</a
-                >.
-              </p>
+              <?php
+              switch ($currentMenu) {
+                  case 'products':
+                      include 'app-ecommerce-product-list.php';
+                      break;
+              }
+              ?>
             </div>
             <!-- / Content -->
 
