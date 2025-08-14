a8c1dea2e9a60badc2b3d57e6614ca9541905dd9
FOX <thienduongvangem.ntt@gmail.com>
2025-08-14 09:07:56 +0700

    Update index.php

--- html/vertical-menu-template-no-customizer/index.php
+++ html/vertical-menu-template-no-customizer/index.php
@@ -192,7 +192,7 @@ $user = $_SESSION['auth']['user'] ?? 'user';
                         </li>
                         <li class="menu-item">
                             <a href="layouts-container.html" class="menu-link">
-                                <div data-i18n="Container">Xlsx Settings</div>
+                                <div data-i18n="Container">Export Settings</div>
                             </a>
                         </li>
                     </ul>
