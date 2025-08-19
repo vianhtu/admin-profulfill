27dc401455585679af6299e3a183077f76f90b2b
FOX <thienduongvangem.ntt@gmail.com>
2025-08-19 21:18:21 +0700

    Update app-ecommerce-product-list.php

--- html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
@@ -63,7 +63,7 @@ $info = getAuthorsProductInfo();
                             <p class="mb-1">Your Profit</p>
                             <h4 class="mb-1"><?php echo number_format($info['author_items'] * 60); ?> vnd</h4>
                             <p class="mb-0">
-                                <span class="me-2">0 vnd paid</span><span class="badge bg-label-danger">-3.5%</span>
+                                <span class="me-2">Paid: -0.0 (vnd)</span>
                             </p>
                         </div>
                         <span class="avatar p-2">
