cd55a8298b6a645a291e86eac9d08a2db11196cc
FOX <thienduongvangem.ntt@gmail.com>
2025-08-19 20:58:12 +0700

    Update app-ecommerce-product-list.php

--- html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
@@ -11,7 +11,7 @@ $info = getAuthorsProductInfo();
                             class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-4 pb-sm-0">
                         <div>
                             <p class="mb-1">Total Products</p>
-                            <h4 class="mb-1"><?php echo $info['total_items']; ?></h4>
+                            <h4 class="mb-1"><?php echo number_format($info['total_items']); ?></h4>
                             <p class="mb-0">
                                 <span class="me-2">5k orders</span><span class="badge bg-label-success">+5.7%</span>
                             </p>
