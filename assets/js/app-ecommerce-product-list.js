593ef7e2fb1829ac3c2a1a8af03902263c1b5e98
FOX <thienduongvangem.ntt@gmail.com>
2025-08-19 11:49:32 +0700

    Update app-ecommerce-product-list.js

--- assets/js/app-ecommerce-product-list.js
+++ assets/js/app-ecommerce-product-list.js
@@ -646,7 +646,7 @@ function initProductTable(){
                 });
 
                 //sites.
-                let typesHTML = '<label class="form-label">Remove listed on sites</label>';
+                let typesHTML = '<div class="mb-2"><label class="form-label">Remove listed on sites</label></div>';
                 $.each(sitesObj, function(key, value) {
                     typesHTML += '<div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" value="'+key+'" id="check'+key+'"><label class="form-check-label">'+value.title+'</label></div>';
                 });
