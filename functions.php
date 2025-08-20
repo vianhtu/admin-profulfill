4423ae861be7050f53c14956481051ef5c1e3699
FOX <thienduongvangem.ntt@gmail.com>
2025-08-20 15:50:42 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,9 +3,7 @@ function renderMenu($currentMenu) {
 	$menuItems = [
 		'Dashboards' => [
 			'icon' => 'tabler-smart-home',
-			'sub' => [
-				'analytics' => 'Analytics'
-			]
+			'sub' => ['analytics' => 'Analytics']
 		],
 		'eCommerce' => [
 			'icon' => 'tabler-shopping-cart',
@@ -20,42 +18,48 @@ function renderMenu($currentMenu) {
 				'export-settings' => 'Export Settings'
 			]
 		],
-		'Users' => ['icon' => 'tabler-users', 'sub' => []]
+		'Users' => ['icon' => 'tabler-users', 'link' => 'users']
 	];
 
 	foreach ($menuItems as $mainLabel => $mainData) {
-		$isOpen = false;
-		$subMenuHtml = '';
-
-		foreach ($mainData['sub'] as $key => $value) {
-			$label = is_array($value) ? $value['label'] : $value;
-			$target = is_array($value) && isset($value['target']) ? 'target="_blank"' : '';
-			$activeClass = ($currentMenu === $key) ? 'active' : '';
-			if ($activeClass) $isOpen = true;
-
-			$subMenuHtml .= <<<HTML
-                <li class="menu-item {$activeClass}">
-                    <a href="index.php?menu={$key}" class="menu-link" {$target}>
-                        <div data-i18n="{$label}">{$label}</div>
-                    </a>
-                </li>
-            HTML;
-		}
-
-		$openClass = $isOpen ? 'active open' : '';
 		$icon = $mainData['icon'];
 
-		echo <<<HTML
-            <li class="menu-item {$openClass}">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon icon-base ti {$icon}"></i>
-                    <div data-i18n="{$mainLabel}">{$mainLabel}</div>
+		// Nếu có submenu
+		if (!empty($mainData['sub'])) {
+			$subMenuHtml = '';
+			$isOpen = false;
+			foreach ($mainData['sub'] as $key => $value) {
+				$label = is_array($value) ? $value['label'] : $value;
+				$target = is_array($value) && isset($value['target']) ? 'target="_blank"' : '';
+				$activeClass = ($currentMenu === $key) ? 'active' : '';
+				if ($activeClass) $isOpen = true;
+
+				$subMenuHtml .= "<li class='menu-item {$activeClass}'>
+                    <a href='index.php?menu={$key}' class='menu-link' {$target}>
+                        <div data-i18n='{$label}'>{$label}</div>
+                    </a>
+                </li>";
+			}
+			$openClass = $isOpen ? 'active open' : '';
+			echo "<li class='menu-item {$openClass}'>
+                <a href='javascript:void(0);' class='menu-link menu-toggle'>
+                    <i class='menu-icon icon-base ti {$icon}'></i>
+                    <div data-i18n='{$mainLabel}'>{$mainLabel}</div>
                 </a>
-                <ul class="menu-sub">
-                    {$subMenuHtml}
-                </ul>
-            </li>
-        HTML;
+                <ul class='menu-sub'>{$subMenuHtml}</ul>
+            </li>";
+		}
+		// Nếu không có submenu
+		else {
+			$link = $mainData['link'] ?? '#';
+			$activeClass = ($currentMenu === $link) ? 'active' : '';
+			echo "<li class='menu-item {$activeClass}'>
+                <a href='index.php?menu={$link}' class='menu-link'>
+                    <i class='menu-icon icon-base ti {$icon}'></i>
+                    <div data-i18n='{$mainLabel}'>{$mainLabel}</div>
+                </a>
+            </li>";
+		}
 	}
 }
 
