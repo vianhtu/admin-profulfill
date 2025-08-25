0f6f926435c4a46b9150c066f42d7489423362ca
FOX <thienduongvangem.ntt@gmail.com>
2025-08-25 16:01:48 +0700

    Update app-ecommerce-product-list.js

--- assets/js/app-ecommerce-product-list.js
+++ assets/js/app-ecommerce-product-list.js
@@ -258,7 +258,7 @@ function initProductTable(){
                     features: [
                         {
                             pageLength: {
-                                menu: [7, 10, 25, 50, 100, 500, 1000, 2000],
+                                menu: [10, 25, 50, 100, 500, 1000, 2000],
                                 text: '_MENU_'
                             },
                             buttons: [
