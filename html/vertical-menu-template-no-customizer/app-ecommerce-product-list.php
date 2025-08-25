0fe952bfa0a24a1b39c65043e54e26be762572e5
FOX <thienduongvangem.ntt@gmail.com>
2025-08-25 09:47:43 +0700

    Update app-ecommerce-product-list.php

--- html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-product-list.php
@@ -93,7 +93,7 @@ $info = getAuthorsProductInfo();
             <div class="col-md-3 product_store"></div>
             <div class="col-md-2 product_from_date"></div>
             <div class="col-md-2 product_to_date"></div>
-            <div class="col-md-4 product_sites"></div>
+            <div class="col-md-4 product_accounts"></div>
             <div class="col-md-4 product_sites"></div>
         </div>
         </div>
