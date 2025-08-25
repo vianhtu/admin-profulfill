e2a5cbb02f5a1a041d6adfd84107e602051eb0e0
FOX <thienduongvangem.ntt@gmail.com>
2025-08-25 15:18:44 +0700

    Update app-ecommerce-product-list.js

--- assets/js/app-ecommerce-product-list.js
+++ assets/js/app-ecommerce-product-list.js
@@ -625,13 +625,12 @@ function initProductTable(){
                     typesHTML += '<div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" value="'+key+'" id="check'+key+'"><label class="form-check-label">'+value.title+'</label></div>';
                 });
                 $('.product_sites').html(typesHTML);
+                $('#maxDate,.product_sites input').on('change', function () {
+                    tableApi.draw();
+                });
 
                 // Export.
                 getMultipleSelect('export_accounts', 'accountsExport', 'Select Account', 'filter-accounts', false);
-
-                $('#maxDate,#storeFilter,#accountsFilter,.product_sites input').on('change', function () {
-                    tableApi.draw();
-                });
             }
         });
     }
@@ -715,6 +714,8 @@ function getMultipleSelect(div_class, select_id, select_label, action, multiple 
             searching: () => 'Đang tìm...',
             noResults: () => 'Không có kết quả'
         }
+    }).on('change', function () {
+        tableApi.draw();
     });
 }
 
