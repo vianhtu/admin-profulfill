10e53ccb16930afc220f158a107e43e85610569b
FOX <thienduongvangem.ntt@gmail.com>
2025-08-26 11:20:29 +0700

    Update app-ecommerce-product-add.js

--- assets/js/app-ecommerce-product-add.js
+++ assets/js/app-ecommerce-product-add.js
@@ -2,46 +2,6 @@
  * App eCommerce Add Product Script
  */
 'use strict';
-
-//Javascript to handle the e-commerce product add page
-
-(function () {
-  // previewTemplate: Updated Dropzone default previewTemplate
-  // ! Don't change it unless you really know what you are doing
-  const previewTemplate = `<div class="dz-preview dz-file-preview">
-<div class="dz-details">
-  <div class="dz-thumbnail">
-    <img data-dz-thumbnail>
-    <span class="dz-nopreview">No preview</span>
-    <div class="dz-success-mark"></div>
-    <div class="dz-error-mark"></div>
-    <div class="dz-error-message"><span data-dz-errormessage></span></div>
-    <div class="progress">
-      <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" data-dz-uploadprogress></div>
-    </div>
-  </div>
-  <div class="dz-filename" data-dz-name></div>
-  <div class="dz-size" data-dz-size></div>
-</div>
-</div>`;
-
-  // ? Start your code from here
-
-  // Basic Dropzone
-
-  const dropzoneBasic = document.querySelector('#dropzone-basic');
-  if (dropzoneBasic) {
-    const myDropzone = new Dropzone(dropzoneBasic, {
-      previewTemplate: previewTemplate,
-      parallelUploads: 1,
-      maxFilesize: 5,
-      acceptedFiles: '.xlsx',
-      addRemoveLinks: true,
-      maxFiles: 1
-    });
-  }
-})();
-
 //Jquery to handle the e-commerce product add page
 
 $(function () {
@@ -103,6 +63,42 @@ $(function () {
 });
 
 document.addEventListener('DOMContentLoaded', function (e) {
+
+    // previewTemplate: Updated Dropzone default previewTemplate
+    // ! Don't change it unless you really know what you are doing
+    const previewTemplate = `<div class="dz-preview dz-file-preview">
+    <div class="dz-details">
+      <div class="dz-thumbnail">
+        <img data-dz-thumbnail>
+        <span class="dz-nopreview">No preview</span>
+        <div class="dz-success-mark"></div>
+        <div class="dz-error-mark"></div>
+        <div class="dz-error-message"><span data-dz-errormessage></span></div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" data-dz-uploadprogress></div>
+        </div>
+      </div>
+      <div class="dz-filename" data-dz-name></div>
+      <div class="dz-size" data-dz-size></div>
+    </div>
+    </div>`;
+
+    // ? Start your code from here
+
+    // Basic Dropzone
+
+    const dropzoneBasic = document.querySelector('#dropzone-basic');
+    if (dropzoneBasic) {
+        const myDropzone = new Dropzone(dropzoneBasic, {
+            previewTemplate: previewTemplate,
+            parallelUploads: 1,
+            maxFilesize: 5,
+            acceptedFiles: '.xlsx',
+            addRemoveLinks: true,
+            maxFiles: 1
+        });
+    }
+
     getMultipleSelect('export_accounts', 'accountsExport', 'Select Account', 'filter-accounts', false);
     $('#export_submit').on('click', function (e) {
         let isValid = true;
