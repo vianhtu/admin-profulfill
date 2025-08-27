f97531964ffc97cc7f935a8f058d82e0e4c13b17
FOX <thienduongvangem.ntt@gmail.com>
2025-08-27 17:02:44 +0700

    Update app-xlsx-list.js

--- assets/js/app-xlsx-list.js
+++ assets/js/app-xlsx-list.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
                   <i class="icon-base ti tabler-trash icon-22px"></i>
                 </a>
                 <a href="index.php?menu=exports_add&id=${full['id']}" class="btn btn-text-secondary rounded-pill waves-effect btn-icon">
-                  <i class="icon-base ti tabler-eye icon-22px"></i>
+                  <i class="icon-base ti tabler-edit icon-22px"></i>
                 </a>
                 <a href="javascript:;" class="btn btn-text-secondary rounded-pill waves-effect btn-icon duplicate-record">
                   <i class="icon-base ti tabler-copy-check icon-22px"></i>
