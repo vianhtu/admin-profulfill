8009197a3e6579f3fa97396b3d06dbb1a5092737
FOX <thienduongvangem.ntt@gmail.com>
2025-09-05 11:58:08 +0700

    ok

--- assets/js/app-ecommerce-product-list.js
+++ assets/js/app-ecommerce-product-list.js
@@ -186,7 +186,7 @@ function initProductTable(){
                     targets: 6,
                     render: function (data, type, full, meta) {
                         let badge = full['badge'];
-                        if(badge === 'null'){
+                        if(badge === null){
                             badge = '';
                         }
                         return '<span>' + badge + '</span>';
