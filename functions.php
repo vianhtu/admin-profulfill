422eb9b2a461725a911933f64a76e2ba86937821
FOX <thienduongvangem.ntt@gmail.com>
2025-09-08 14:36:21 +0700

    ok

--- functions.php
+++ functions.php
@@ -546,7 +546,7 @@ function getDownloadTable(): array {
     while ( $row = $rs->fetch_assoc() ) {
         $data[] = [
             "id"            => $row['ID'],
-            "full_name"     => $row['name'] . '(' . $row['date'] . ')',
+            "full_name"     => $row['name'] . ' (' . $row['date'] . ')',
             "email"       => $row['email'],
             "site_id"       => $row['site_id'],
             "authors_id"    => 1,
