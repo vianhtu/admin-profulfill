8da60c7c3bfcc9282fed3652c6940cf5ac7ce119
FOX <thienduongvangem.ntt@gmail.com>
2025-09-15 23:27:04 +0700

    ok

--- html/vertical-menu-template-no-customizer/extended-ui-timeline-basic.php
+++ html/vertical-menu-template-no-customizer/extended-ui-timeline-basic.php
@@ -1,6 +1,16 @@
 <?php
 require __DIR__ . '/../../functions-telnyx.php';
 $sms = get_sms();
+$colors = [
+        'timeline-point-primary',
+        'timeline-point-success',
+        'timeline-point-danger',
+        'timeline-point-warning',
+        'timeline-point-info',
+        'timeline-point-secondary'
+];
+// Chọn ngẫu nhiên 1 class
+$randomColor = $colors[array_rand($colors)];
 ?>
 <div class="row gy-6">
     <!-- Timeline Basic-->
@@ -11,7 +21,7 @@ $sms = get_sms();
                 <ul class="timeline mb-0">
                     <?php foreach ($sms as $value): ?>
                         <li class="timeline-item timeline-item-transparent">
-                            <span class="timeline-point timeline-point-primary"></span>
+                            <span class="timeline-point <?= $randomColor; ?>"></span>
                             <div class="timeline-event">
                                 <div class="timeline-header mb-3">
                                     <h6 class="mb-0"><?= $value['name']; ?> <?= $value['number']; ?></h6>
