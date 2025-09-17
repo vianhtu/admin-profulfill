3b1efc3e48ca2f36147346ba75d87ccd2b91bcf7
FOX <thienduongvangem.ntt@gmail.com>
2025-09-17 14:59:36 +0700

    ok

--- html/vertical-menu-template-no-customizer/index.php
+++ html/vertical-menu-template-no-customizer/index.php
@@ -73,6 +73,7 @@ if (empty($_SESSION['csrf_token'])) {
     <!-- Helpers -->
     <script src="../../assets/vendor/js/helpers.js"></script>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <script src="../../assets/vendor/js/template-customizer.js"></script>
 
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
