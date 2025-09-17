c8230ccc0a97d555c8364d4b13742ea0f2ab5478
FOX <thienduongvangem.ntt@gmail.com>
2025-09-17 22:20:15 +0700

    ok

--- assets/js/app-ecommerce-order-list.js
+++ assets/js/app-ecommerce-order-list.js
@@ -58,7 +58,7 @@ function getItemsRow(orderItems, colCount) {
 
         html += `
           <tr>
-            <td style="width: 100px">${row_image}</td>
+            <td style="width: 350px">${row_image}</td>
             <td><small>${item.Quantity}</small></td>
             <td><small>${item.Cost.Amount} ${item.Cost.CurrencyCode}</small></td>
             <td>${row_custom}</td>
