1f82a10d6ae175fc18be2db90986be44e1b6c9c3
FOX <thienduongvangem.ntt@gmail.com>
2025-09-17 20:22:41 +0700

    ok

--- assets/js/app-ecommerce-order-list.js
+++ assets/js/app-ecommerce-order-list.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
                   row_output += '<div class="d-flex justify-content-start align-items-center product-name">' +
                       '<div class="avatar-wrapper">' +
                       '<div class="avatar avatar me-2 me-sm-4 rounded-2 bg-label-secondary">' +
-                      '<img src="'+item.Image+'" alt="'+item.Title+'" class="rounded" style="cursor:pointer;" onclick="showImageModal(\''+getFullSizeImage(item.Image)+'\')">' +
+                      '<img src="'+item.Image+'" title="'+item.Title+'" alt="'+item.Title+'" class="rounded" style="cursor:pointer;" onclick="showImageModal(\''+getFullSizeImage(item.Image)+'\')">' +
                       '</div>' +
                       '</div>' +
                       '<div class="d-flex flex-column">' +
