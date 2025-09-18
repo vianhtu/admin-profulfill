d005f8f1a7616516571c15307648c7a8abe880ba
FOX <thienduongvangem.ntt@gmail.com>
2025-09-18 11:24:26 +0700

    ok

--- assets/js/app-ecommerce-order-list.js
+++ assets/js/app-ecommerce-order-list.js
@@ -176,7 +176,7 @@ function initTable(){
                         return '<a href="app-ecommerce-order-details.html">' +
                                 '<span class="text-nowrap">#' + order_id + '</span>' +
                             '</a>' +
-                            '<span class="text-nowrap">'+sitesObj[full['account_name']]+'('+full['account_name']+')</span>';
+                            '<span class="text-nowrap">'+sitesObj[full['account_id']]+'('+full['account_name']+')</span>';
                     }
                 },
                 {
