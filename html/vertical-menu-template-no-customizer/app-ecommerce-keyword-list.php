513ade35ad51df1775c6b29e8549a07435977ed9
FOX <thienduongvangem.ntt@gmail.com>
2025-09-19 16:07:11 +0700

    ok

--- html/vertical-menu-template-no-customizer/app-ecommerce-keyword-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-keyword-list.php
@@ -33,6 +33,16 @@
         <form class="ecommerce-customer-add pt-0" id="eCommerceCustomerAddForm" onsubmit="return false">
             <div class="ecommerce-customer-add-basic mb-4">
                 <h6 class="mb-6">Information</h6>
+                <div class="mb-6 form-control-validation">
+                    <label class="form-label" for="add-name">Name*</label>
+                    <input
+                            type="text"
+                            class="form-control"
+                            id="add-name"
+                            placeholder="Apple"
+                            name="customerName"
+                            aria-label="Apple"/>
+                </div>
                 <div class="form-control-validation">
                     <label class="form-label" for="select-status">Status</label>
                     <select id="select-status" class="select2 form-select">
@@ -42,123 +52,7 @@
                         <option value="3">Cannot be Used</option>
                     </select>
                 </div>
-                <div class="mb-6 form-control-validation">
-                    <label class="form-label" for="ecommerce-customer-add-name">Name*</label>
-                    <input
-                            type="text"
-                            class="form-control"
-                            id="ecommerce-customer-add-name"
-                            placeholder="John Doe"
-                            name="customerName"
-                            aria-label="John Doe"/>
-                </div>
-                <div class="mb-6 form-control-validation">
-                    <label class="form-label" for="ecommerce-customer-add-email">Email*</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-email"
-                            class="form-control"
-                            placeholder="john.doe@example.com"
-                            aria-label="john.doe@example.com"
-                            name="customerEmail"/>
-                </div>
-                <div>
-                    <label class="form-label" for="ecommerce-customer-add-contact">Mobile</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-contact"
-                            class="form-control phone-mask"
-                            placeholder="+(123) 456-7890"
-                            aria-label="+(123) 456-7890"
-                            name="customerContact"/>
-                </div>
             </div>
-
-            <div class="ecommerce-customer-add-shiping mb-6 pt-4">
-                <h6 class="mb-6">Shipping Information</h6>
-                <div class="mb-6">
-                    <label class="form-label" for="ecommerce-customer-add-address">Address Line 1</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-address"
-                            class="form-control"
-                            placeholder="45 Roker Terrace"
-                            aria-label="45 Roker Terrace"
-                            name="customerAddress1"/>
-                </div>
-                <div class="mb-6">
-                    <label class="form-label" for="ecommerce-customer-add-address-2">Address Line 2</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-address-2"
-                            class="form-control"
-                            aria-label="address2"
-                            name="customerAddress2"/>
-                </div>
-                <div class="mb-6">
-                    <label class="form-label" for="ecommerce-customer-add-town">Town</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-town"
-                            class="form-control"
-                            placeholder="New York"
-                            aria-label="New York"
-                            name="customerTown"/>
-                </div>
-                <div class="col-12 mb-6">
-                    <label class="form-label" for="ecommerce-customer-add-state">State / Province</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-state"
-                            class="form-control"
-                            placeholder="Southern tip"
-                            aria-label="Southern tip"
-                            name="customerState"/>
-                </div>
-                <div class="col-12 mb-6">
-                    <label class="form-label" for="ecommerce-customer-add-post-code">Post Code</label>
-                    <input
-                            type="text"
-                            id="ecommerce-customer-add-post-code"
-                            class="form-control"
-                            placeholder="734990"
-                            aria-label="734990"
-                            name="pin"
-                            pattern="[0-9]{8}"
-                            maxlength="8"/>
-                </div>
-                <div>
-                    <label class="form-label" for="ecommerce-customer-add-country">Country</label>
-                    <select id="ecommerce-customer-add-country" class="select2 form-select">
-                        <option value="">Select</option>
-                        <option value="Australia">Australia</option>
-                        <option value="Bangladesh">Bangladesh</option>
-                        <option value="Belarus">Belarus</option>
-                        <option value="Brazil">Brazil</option>
-                        <option value="Canada">Canada</option>
-                        <option value="China">China</option>
-                        <option value="France">France</option>
-                        <option value="Germany">Germany</option>
-                        <option value="India">India</option>
-                        <option value="Indonesia">Indonesia</option>
-                        <option value="Israel">Israel</option>
-                        <option value="Italy">Italy</option>
-                        <option value="Japan">Japan</option>
-                        <option value="Korea">Korea, Republic of</option>
-                        <option value="Mexico">Mexico</option>
-                        <option value="Philippines">Philippines</option>
-                        <option value="Russia">Russian Federation</option>
-                        <option value="South Africa">South Africa</option>
-                        <option value="Thailand">Thailand</option>
-                        <option value="Turkey">Turkey</option>
-                        <option value="Ukraine">Ukraine</option>
-                        <option value="United Arab Emirates">United Arab Emirates</option>
-                        <option value="United Kingdom">United Kingdom</option>
-                        <option value="United States">United States</option>
-                    </select>
-                </div>
-            </div>
-
             <div class="d-sm-flex mb-6">
                 <div class="me-auto mb-2 mb-md-0">
                     <h6 class="mb-1">Use as a billing address?</h6>
