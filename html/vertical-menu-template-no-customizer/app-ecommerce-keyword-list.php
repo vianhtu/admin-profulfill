e70aad516e52791fd0b688cd9d1964ac30ce7a60
FOX <thienduongvangem.ntt@gmail.com>
2025-09-19 10:30:17 +0700

    ok

--- html/vertical-menu-template-no-customizer/app-ecommerce-keyword-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-keyword-list.php
@@ -8,7 +8,7 @@
                 <th></th>
                 <th>name</th>
                 <th>status</th>
-                <th data-align="right">actions</th>
+                <th class="text-end">actions</th>
             </tr>
             </thead>
         </table>
