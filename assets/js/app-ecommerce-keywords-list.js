e0646d7ec5998245c57e3f21e6e0234722549fb3
FOX <thienduongvangem.ntt@gmail.com>
2025-09-19 08:47:31 +0700

    ok

--- assets/js/app-ecommerce-keywords-list.js
+++ assets/js/app-ecommerce-keywords-list.js
@@ -39,81 +39,16 @@ function replaceAmazonImageSize(url, newSize) {
     return url.replace(/SX\d+/, `SX${newSize}`);
 }
 
-// Hàm tạo HTML cho bảng con
-function getItemsRow(orderItems, colCount) {
-    let html = '';
-
-    orderItems.forEach(item => {
-        const row_image = `
-          <div class="d-flex justify-content-start align-items-center product-name">
-            <div class="avatar-wrapper">
-              <div class="avatar avatar me-2 me-sm-4 rounded-2 bg-label-secondary" style="width:80px; height:80px;">
-                <img src="${replaceAmazonImageSize(item.Image, 150)}" 
-                     title="${item.Title}" 
-                     alt="${item.Title}" 
-                     class="rounded img-fluid" 
-                     style="cursor:pointer;" 
-                     onclick="showImageModal('${getFullSizeImage(item.Image)}')">
-              </div>
-            </div>
-            <div class="d-flex flex-column">
-              <small>asin: <a href="https://www.amazon.com/dp/${item.ASIN}" target="_blank">${item.ASIN}</a></small>
-              <small>sku: ${item.SKU}</small>
-              <small>qlt: ${item.Quantity}</small>
-              <small>price: ${item.Cost.Amount} ${item.Cost.CurrencyCode}</small>
-            </div>
-          </div>`;
-
-        let row_custom = '<div class="d-flex flex-column">';
-        item.Customizations.forEach(m => {
-            m.Modifications.forEach(values => {
-                row_custom += `<small>${values.Name}: ${values.Value}</small>`;
-            });
-        });
-        row_custom += '</div>';
-
-        html += `
-          <tr class="child-row">
-            <td style="display: none;"></td>
-            <td></td>
-            <td>${row_image}</td>
-            <td>${row_custom}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>`;
-    });
-
-    html += '';
-
-    return html;
-}
 // Datatable (js)
 function initTable(){
-    let borderColor, bodyBg, headingColor;
-
-    borderColor = config.colors.borderColor;
-    bodyBg = config.colors.bodyBg;
-    headingColor = config.colors.headingColor;
-
     // Variable declaration for table
-
     const dt_order_table = document.querySelector('.datatables-order'),
         statusObj = {
             Unshipped: { title: 'Unshipped', class: 'bg-label-warning' },
             2: { title: 'Delivered', class: 'bg-label-success' },
             3: { title: 'Out for Delivery', class: 'bg-label-primary' },
             4: { title: 'Ready to Pickup', class: 'bg-label-info' }
-        },
-        paymentObj = {
-            1: { title: 'Paid', class: 'text-success' },
-            2: { title: 'Pending', class: 'text-warning' },
-            3: { title: 'Failed', class: 'text-danger' },
-            4: { title: 'Cancelled', class: 'text-secondary' }
-        };
+        }
 
     // E-commerce Products datatable
 
@@ -170,94 +105,44 @@ function initTable(){
                     // Order ID
                     targets: 2,
                     render: function (data, type, full, meta) {
-                        const order_id = full['host_id'];
-                        const items = JSON.parse(full['items']);
-                        // Creates full output for row
-                        return '<div class="d-flex flex-column">' +
-                            '<a href="app-ecommerce-order-details.html">' +
-                                '<span class="text-nowrap">#' + order_id + '</span>' +
-                            '</a><small>'+sitesObj[full['account_id']].title+' ('+full['account_name']+')</small>' +
-                            '</div>';
+                        return '';
                     }
                 },
                 {
                     targets: 3,
                     render: function (data, type, full, meta) {
-                        const date = new Date(full['purchase_date']);
-                        const formattedDate = date.toLocaleDateString('en-US', {
-                            month: 'short',
-                            day: 'numeric',
-                            year: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            hour12: false,
-                            timeZone: 'Asia/Ho_Chi_Minh'
-                        });
-                        return `<span class="text-nowrap">${formattedDate}</span>`;
+                        return '';
                     }
                 },
                 {
                     targets: 4,
                     render: function (data, type, full, meta) {
-                        return '<h6 class="text-nowrap mb-0">$'+full['total_price']+'</h6>';
+                        return '';
                     }
                 },
                 {
                     targets: 5,
                     render: function (data, type, full, meta) {
-                        const date = new Date(full['ship_date']);
-                        const formattedDate = date.toLocaleString('en-US', {
-                            month: 'short',
-                            day: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            hour12: false,
-                            timeZone: 'Asia/Ho_Chi_Minh'
-                        });
-                        return `<span class="text-nowrap">${formattedDate}</span>`;
+                        return '';
                     }
                 },
                 {
                     targets: 6,
                     render: function (data, type, full, meta) {
-                        const date = new Date(full['delivery_date']);
-                        const formattedDate = date.toLocaleString('en-US', {
-                            month: 'short',
-                            day: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            hour12: false,
-                            timeZone: 'Asia/Ho_Chi_Minh'
-                        });
-                        return `<span class="text-nowrap">${formattedDate}</span>`;
+                        return '';
                     }
                 },
                 {
                     targets: 7,
                     render: function (data, type, full, meta) {
-                        const status = full['status'];
-                        const statusInfo = statusObj[status];
-                        if (statusInfo) {
-                            return `
-                <span class="badge px-2 ${statusInfo.class} text-capitalized">
-                  ${statusInfo.title}
-                </span>`;
-                        }
-                        return data;
+                        return '';
                     }
                 },
                 {
                     targets: 8,
                     responsivePriority: 1,
                     render: function (data, type, full, meta) {
-                        const street_address_2 = full['street_address_2'] ? '<small class="text-truncate d-none d-sm-block">'+full['street_address_2']+'</small>' : '';
-                        return '<div class="d-flex flex-column">' +
-                            '<h6 class="text-nowrap mb-0">'+full['full_name']+'</h6>' +
-                            '<small class="text-truncate d-none d-sm-block">'+full['street_address_1']+'</small>' +
-                            street_address_2 +
-                            '<small class="text-truncate d-none d-sm-block">'+full['city']+', '+full['state']+' '+ full['zip_code'] +'</small>' +
-                            '<small class="text-truncate d-none d-sm-block">'+full['country']+'</small>' +
-                            '</div>';
+                        return '';
                     }
                 },
                 {
@@ -305,133 +190,8 @@ function initTable(){
                                 {
                                     extend: 'collection',
                                     className: 'btn btn-label-primary dropdown-toggle',
-                                    text: '<span class="d-flex align-items-center gap-1"><i class="icon-base ti tabler-upload icon-xs"></i> <span class="d-none d-sm-inline-block">Export</span></span>',
+                                    text: '<span class="d-flex align-items-center gap-1"><i class="icon-base ti tabler-upload icon-xs"></i> <span class="d-none d-sm-inline-block">Actions</span></span>',
                                     buttons: [
-                                        {
-                                            extend: 'print',
-                                            text: `<span class="d-flex align-items-center"><i class="icon-base ti tabler-printer me-1"></i>Print</span>`,
-                                            className: 'dropdown-item',
-                                            exportOptions: {
-                                                columns: [3, 4, 5, 6, 7],
-                                                format: {
-                                                    body: function (inner, coldex, rowdex) {
-                                                        if (inner.length <= 0) return inner;
-                                                        const el = new DOMParser().parseFromString(inner, 'text/html').body.childNodes;
-                                                        let result = '';
-                                                        el.forEach(item => {
-                                                            if (item.classList && item.classList.contains('user-name')) {
-                                                                result += item.lastChild.firstChild.textContent;
-                                                            } else {
-                                                                result += item.textContent || item.innerText || '';
-                                                            }
-                                                        });
-                                                        return result;
-                                                    }
-                                                }
-                                            },
-                                            customize: function (win) {
-                                                win.document.body.style.color = headingColor;
-                                                win.document.body.style.borderColor = borderColor;
-                                                win.document.body.style.backgroundColor = bodyBg;
-                                                const table = win.document.body.querySelector('table');
-                                                table.classList.add('compact');
-                                                table.style.color = 'inherit';
-                                                table.style.borderColor = 'inherit';
-                                                table.style.backgroundColor = 'inherit';
-                                            }
-                                        },
-                                        {
-                                            extend: 'csv',
-                                            text: `<span class="d-flex align-items-center"><i class="icon-base ti tabler-file me-1"></i>Csv</span>`,
-                                            className: 'dropdown-item',
-                                            exportOptions: {
-                                                columns: [3, 4, 5, 6, 7],
-                                                format: {
-                                                    body: function (inner, coldex, rowdex) {
-                                                        if (inner.length <= 0) return inner;
-                                                        const el = new DOMParser().parseFromString(inner, 'text/html').body.childNodes;
-                                                        let result = '';
-                                                        el.forEach(item => {
-                                                            if (item.classList && item.classList.contains('user-name')) {
-                                                                result += item.lastChild.firstChild.textContent;
-                                                            } else {
-                                                                result += item.textContent || item.innerText || '';
-                                                            }
-                                                        });
-                                                        return result;
-                                                    }
-                                                }
-                                            }
-                                        },
-                                        {
-                                            extend: 'excel',
-                                            text: `<span class="d-flex align-items-center"><i class="icon-base ti tabler-upload me-1"></i>Excel</span>`,
-                                            className: 'dropdown-item',
-                                            exportOptions: {
-                                                columns: [3, 4, 5, 6, 7],
-                                                format: {
-                                                    body: function (inner, coldex, rowdex) {
-                                                        if (inner.length <= 0) return inner;
-                                                        const el = new DOMParser().parseFromString(inner, 'text/html').body.childNodes;
-                                                        let result = '';
-                                                        el.forEach(item => {
-                                                            if (item.classList && item.classList.contains('user-name')) {
-                                                                result += item.lastChild.firstChild.textContent;
-                                                            } else {
-                                                                result += item.textContent || item.innerText || '';
-                                                            }
-                                                        });
-                                                        return result;
-                                                    }
-                                                }
-                                            }
-                                        },
-                                        {
-                                            extend: 'pdf',
-                                            text: `<span class="d-flex align-items-center"><i class="icon-base ti tabler-file-text me-1"></i>Pdf</span>`,
-                                            className: 'dropdown-item',
-                                            exportOptions: {
-                                                columns: [3, 4, 5, 6, 7],
-                                                format: {
-                                                    body: function (inner, coldex, rowdex) {
-                                                        if (inner.length <= 0) return inner;
-                                                        const el = new DOMParser().parseFromString(inner, 'text/html').body.childNodes;
-                                                        let result = '';
-                                                        el.forEach(item => {
-                                                            if (item.classList && item.classList.contains('user-name')) {
-                                                                result += item.lastChild.firstChild.textContent;
-                                                            } else {
-                                                                result += item.textContent || item.innerText || '';
-                                                            }
-                                                        });
-                                                        return result;
-                                                    }
-                                                }
-                                            }
-                                        },
-                                        {
-                                            extend: 'copy',
-                                            text: `<i class="icon-base ti tabler-copy me-1"></i>Copy`,
-                                            className: 'dropdown-item',
-                                            exportOptions: {
-                                                columns: [3, 4, 5, 6, 7],
-                                                format: {
-                                                    body: function (inner, coldex, rowdex) {
-                                                        if (inner.length <= 0) return inner;
-                                                        const el = new DOMParser().parseFromString(inner, 'text/html').body.childNodes;
-                                                        let result = '';
-                                                        el.forEach(item => {
-                                                            if (item.classList && item.classList.contains('user-name')) {
-                                                                result += item.lastChild.firstChild.textContent;
-                                                            } else {
-                                                                result += item.textContent || item.innerText || '';
-                                                            }
-                                                        });
-                                                        return result;
-                                                    }
-                                                }
-                                            }
-                                        }
                                     ]
                                 }
                             ]
@@ -490,17 +250,7 @@ function initTable(){
                 }
             },
             initComplete:function(settings, json) {
-                const api = this.api();
-                const colCount = api.columns().count();
 
-                api.rows().every(function() {
-                    const rowData = this.data();
-                    const items = JSON.parse(rowData.items);
-
-                    // Chèn hàng con ngay sau hàng cha
-                    $(this.node()).after(getItemsRow(items, colCount));
-                    $(this.node()).addClass('shown');
-                });
             }
         });
 
