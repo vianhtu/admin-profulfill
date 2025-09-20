27a9404e685fbd271e7d9a6722e6d1dedf8a1007
FOX <thienduongvangem.ntt@gmail.com>
2025-09-20 15:38:57 +0700

    ok

--- html/vertical-menu-template-no-customizer/app-access-permission.php
+++ html/vertical-menu-template-no-customizer/app-access-permission.php
@@ -8,7 +8,6 @@
                 <th></th>
                 <th>Name</th>
                 <th>Assigned To</th>
-                <th>Created Date</th>
                 <th>Actions</th>
             </tr>
             </thead>
