046ca105d70fcf84598a626fbae74ab22ff1827f
FOX <thienduongvangem.ntt@gmail.com>
2025-09-21 00:12:58 +0700

    ok

--- assets/js/modal-edit-permission.js
+++ assets/js/modal-edit-permission.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function (e) {
     // edit.
     $(document).on('click', '.datatables-permissions .edit', function () {
         const roleId = $(this).data('id');
+        let form = $('#addPermissionModal');
+        // Đổi tiêu đề và nút submit sang chế độ Edit
+        form.find('h3').text('Edit Role');
+        form.find('button[type="submit"]').text('Edit Role');
         $.ajax({
             url: '../../ajax.php?action=get-roles-permissions',
             type: 'POST',
@@ -16,11 +20,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
             data: { id: roleId },
             success: function (res) {
                 if (res.status === 'success') {
-                    let form = $('#addPermissionModal');
-
-                    // Đổi tiêu đề và nút submit sang chế độ Edit
-                    form.find('h3').text('Edit Role');
-                    form.find('button[type="submit"]').text('Edit Role');
 
                     // Gán dữ liệu vào form
                     form.find('#modalPermissionName').val(res.role_name);
@@ -50,15 +49,15 @@ document.addEventListener('DOMContentLoaded', function (e) {
                         form.find('form').append('<input type="hidden" name="role_id">');
                     }
                     form.find('input[name="role_id"]').val(roleId);
-
-                    // Mở modal sau khi load xong
-                    form.modal('show');
                 } else {
-                    alert(res.message);
+                    showAlert('alertPermissionModal', res.message, 'danger');
                 }
+                // Mở modal sau khi load xong
+                form.modal('show');
             },
             error: function () {
-                alert('Không thể kết nối đến máy chủ');
+                showAlert('alertPermissionModal', 'Không thể kết nối đến máy chủ', 'danger');
+                form.modal('show');
             }
         });
     });
