5e184f52a47c4f651432816442ed833808df4216
FOX <thienduongvangem.ntt@gmail.com>
2025-09-23 10:07:36 +0700

    ok

--- assets/js/app-ecommerce-product-copyright.js
+++ assets/js/app-ecommerce-product-copyright.js
@@ -34,11 +34,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
                             <h5 >${item.copyright_warning}</h5>
                             <p class="mt-1">${item.title}</p>
                             <div class="d-flex flex-column flex-md-row gap-4 text-nowrap flex-wrap flex-md-nowrap flex-lg-wrap flex-xxl-nowrap">
-                                <button class="w-100 btn btn-label-danger d-flex align-items-center">
+                                <button class="w-100 btn btn-label-danger d-flex align-items-center item-reject">
                                     <i class="icon-base ti tabler-rotate-clockwise-2 icon-xs align-middle scaleX-n1-rtl me-2"></i>
                                     <span>Reject</span>
                                 </button>
-                                <button class="w-100 btn btn-label-primary d-flex align-items-center">
+                                <button class="w-100 btn btn-label-primary d-flex align-items-center item-approve">
                                     <span class="me-2">Approve</span>
                                     <i class="icon-base ti tabler-chevron-right icon-xs lh-1 scaleX-n1-rtl"></i>
                                 </button>
--- html/vertical-menu-template-no-customizer/app-ecommerce-product-copyright.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-product-copyright.php
@@ -3,23 +3,7 @@
         <div class="card-header d-flex flex-wrap justify-content-between gap-4">
             <div class="card-title mb-0 me-1">
                 <h5 class="mb-0">Products</h5>
-                <p class="mb-0">Total 6 products copyright warning</p>
-            </div>
-            <div
-                    class="d-flex justify-content-md-end align-items-center column-gap-6 flex-sm-row flex-column row-gap-4">
-                <select class="form-select">
-                    <option value="">All Courses</option>
-                    <option value="ui/ux">UI/UX</option>
-                    <option value="seo">SEO</option>
-                    <option value="web">Web</option>
-                    <option value="music">Music</option>
-                    <option value="painting">Painting</option>
-                </select>
-
-                <div class="form-check form-switch my-2 ms-2">
-                    <input type="checkbox" class="form-check-input" id="CourseSwitch" />
-                    <label class="form-check-label text-nowrap mb-0" for="CourseSwitch">Hide completed</label>
-                </div>
+                <p class="mb-0" id="totalWarning">Total 0 products copyright warning</p>
             </div>
         </div>
         <div class="card-body">
