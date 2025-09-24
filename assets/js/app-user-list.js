1af335aeaab9648ddfd083285044b4c0af91e9f4
FOX <thienduongvangem.ntt@gmail.com>
2025-09-24 14:49:37 +0700

    ok

--- assets/js/app-user-list.js
+++ assets/js/app-user-list.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
                 'Manager': '<i class="icon-base ti tabler-automation icon-md text-info me-2"></i>',
                 'Admin': '<i class="icon-base ti tabler-device-desktop icon-md text-danger me-2"></i>'
             };
-            if( role === 'null'){
+            if( role === null){
                 role = 'Admin';
             }
             return (
