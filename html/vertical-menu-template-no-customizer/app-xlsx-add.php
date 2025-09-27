3cfe316ce1f7b8f1766b36c80f8295c197c6d8b7
VI ANH TU <thienduongvangem.ntt@gmail.com>
2025-09-27 10:34:49 +0700

    ok

--- html/vertical-menu-template-no-customizer/app-xlsx-add.php
+++ html/vertical-menu-template-no-customizer/app-xlsx-add.php
@@ -191,9 +191,11 @@ if(!empty($export_data)){
                         <?php renderSelect('export_site', 'Site', $options['sites'], $site_id); ?>
                     </div>
                     <!-- authors -->
+                    <?php if(isAdmin()): ?>
                     <div class="mb-6 col ecommerce-select2-dropdown">
                         <?php renderSelect('export_author', 'Author', $options['authors'], $authors_id); ?>
                     </div>
+                    <?php endif; ?>
                 </div>
             </div>
             <!-- /Organize Card -->
