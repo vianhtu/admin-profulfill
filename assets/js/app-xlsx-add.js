59692fc5f21e9e727e44556cef7bde2a4d6ee7d9
VI ANH TU <thienduongvangem.ntt@gmail.com>
2025-09-27 21:59:54 +0700

    Update app-xlsx-add.js

--- assets/js/app-xlsx-add.js
+++ assets/js/app-xlsx-add.js
@@ -114,6 +114,7 @@ function repeaterOptions(fv){
                 }, 300);
             },
             ready: function (setIndexes) {
+                console.log(formRepeater.data('repeat-count'));
                 if(formRepeater.data('repeat-count') !== '0'){
                     return;
                 }
