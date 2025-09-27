e58e5c559a45f8833f99089ea348ec7db272c85e
VI ANH TU <thienduongvangem.ntt@gmail.com>
2025-09-27 21:36:49 +0700

    ok

--- html/vertical-menu-template-no-customizer/app-xlsx-add.php
+++ html/vertical-menu-template-no-customizer/app-xlsx-add.php
@@ -102,8 +102,12 @@ if(!empty($export_data)){
                                         <select name="" id="form-repeater-<?= $key ?>-1" class="select2 form-select" data-placeholder="Select a option">
                                             <option value=""></option>
                                             <?php foreach ($file_header as $header): ?>
-                                            <?php $selected = $value['text'] == $header['value'] ? ' selected' : ''; ?>
-                                            <option value="<?= $header['column'] ?>"<?= $selected ?>><?= $header['value'] ?></option>
+                                                <?php if(!empty($file_name)): ?>
+                                                    <?php $selected = $value['text'] == $header['value'] ? ' selected' : ''; ?>
+                                                    <option value="<?= $header['column'] ?>"<?= $selected ?>><?= $header['value'] ?></option>
+                                                <?php else: ?>
+                                            <?php endif; ?>
+                                                <option value="<?= $value['location'] ?>" selected><?= $value['text'] ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                     </div>
