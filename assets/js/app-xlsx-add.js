6b7e5b96f482c309c46a85dfae4b7b1b4a7cb4ae
VI ANH TU <thienduongvangem.ntt@gmail.com>
2025-09-27 17:12:16 +0700

    ok

--- assets/js/app-xlsx-add.js
+++ assets/js/app-xlsx-add.js
@@ -5,7 +5,84 @@
 //Jquery to handle the e-commerce product add page
 function init(){
     // get custom header.
+    dropzoneFileUpload();
     repeaterOptions();
+    const fv = formValidate();
+
+    // Select2
+    var select2 = $('#export_type,#export_site,#export_author,#export_sheet_name');
+    if (select2.length) {
+        select2.each(function () {
+            var $this = $(this);
+            $this.wrap('<div class="position-relative"></div>').select2({
+                dropdownParent: $this.parent(),
+            });
+        });
+    }
+
+    // update form.
+    if($('#export_id').val() !== '') {
+        $('#export_file_header, #export_sheet_name').on('change', function () {
+            $('#export_submit').trigger('click');
+        });
+    }
+
+    ajaxSelect2('accountsExport', 'filter-accounts', false);
+
+    // Khi FormValidation validate thành công
+    fv.on('core.form.valid', function() {
+        const $btn = $('#export_submit');
+        const $spinner = $('#loading_spinner');
+
+        // Hiển thị spinner và disable nút
+        $spinner.removeClass('d-none');
+        $btn.prop('disabled', true);
+
+        let id = $('#export_id').val();
+        const formData = new FormData();
+        formData.append('author', $('#export_author').val());
+        formData.append('site', $('#export_site').val());
+        formData.append('type', $('#export_type').val());
+        formData.append('account', $('#accountsExport').val());
+        formData.append('id', id);
+        formData.append('options', JSON.stringify(getRepeaterData()));
+        formData.append('header', $('#export_file_header').val());
+        formData.append('startRow', $('#export_file_start').val());
+        formData.append('sheet_name', $('#export_sheet_name').length ? $('#export_sheet_name').val() : '');
+        formData.append('file', myDropzone.files[0]);
+        formData.append('csrf_token', window.csrfToken);
+
+        $.ajax({
+            url: '../../ajax.php?action=add-xlsx',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                if (response.status === 'inserted' || response.status === 'updated') {
+                    const newId = response.id;
+
+                    // Lấy URL hiện tại và thêm id
+                    const url = new URL(window.location.href);
+                    url.searchParams.set('id', newId);
+
+                    // Reload lại với URL mới
+                    window.location.href = url.toString();
+                } else {
+                    alert('Upload thất bại: ' + response.message);
+                }
+            },
+            error: function (xhr) {
+                console.error('Lỗi:', xhr.responseText);
+                alert('Upload thất bại!');
+            },
+            complete: function () {
+                // Ẩn spinner và bật lại nút
+                $spinner.addClass('d-none');
+                $btn.prop('disabled', false);
+            }
+        });
+    });
 }
 
 function repeaterOptions(){
@@ -128,42 +205,9 @@ function repeaterSelect2Validate($select, fv){
     }
 }
 
-function getRepeaterData() {
-    var data = [];
-    // Mỗi row trong repeater có data-repeater-item
-    $('.form-repeater').find('[data-repeater-item]').each(function () {
-        var selectVal = $(this).find('.form-select').val();
-        var inputVal  = $(this).find('input[type="text"]').val();
-        var selectedText = $(this).find('.form-select option:selected').text();
-        if (selectVal !== null && selectVal !== '' && selectVal !== undefined) {
-            data.push({
-                location: selectVal,
-                text :selectedText,
-                value: inputVal
-            });
-        }
-    });
-
-    return data;
-}
-
-document.addEventListener('DOMContentLoaded', function (e) {
-    init();
-    // Select2
-    var select2 = $('#export_type,#export_site,#export_author,#export_sheet_name');
-    if (select2.length) {
-        select2.each(function () {
-            var $this = $(this);
-            $this.wrap('<div class="position-relative"></div>').select2({
-                dropdownParent: $this.parent(),
-                //placeholder: $this.data('placeholder') // for dynamic placeholder
-            });
-        });
-    }
-
+function formValidate(){
     // form validation
     const formEl = document.getElementById('addXlsxFile');
-    const hiddenInput = document.getElementById('xlsxFilePresent');
     let valid_fields = {
         export_file_header: {
             validators: {
@@ -208,7 +252,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
             }
         },
     };
-    const fv = FormValidation.formValidation(formEl, {
+    return FormValidation.formValidation(formEl, {
         fields: valid_fields,
         plugins: {
             trigger: new FormValidation.plugins.Trigger(),
@@ -220,14 +264,10 @@ document.addEventListener('DOMContentLoaded', function (e) {
             autoFocus: new FormValidation.plugins.AutoFocus()
         }
     });
+}
 
-    // update form.
-    if($('#export_id').val() !== '') {
-        $('#export_file_header, #export_sheet_name').on('change', function () {
-            $('#export_submit').trigger('click');
-        });
-    }
-
+function dropzoneFileUpload(){
+    const hiddenInput = document.getElementById('xlsxFilePresent');
     // previewTemplate: Updated Dropzone default previewTemplate
     // ! Don't change it unless you really know what you are doing
     const previewTemplate = `<div class="dz-preview dz-file-preview">
@@ -246,8 +286,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
       <div class="dz-size" data-dz-size></div>
     </div>
     </div>`;
-
-    // ? Start your code from here
 
     // Basic Dropzone
     let myDropzone;
@@ -286,61 +324,27 @@ document.addEventListener('DOMContentLoaded', function (e) {
             }
         });
     }
+}
 
-    ajaxSelect2('accountsExport', 'filter-accounts', false);
-
-    // Khi FormValidation validate thành công
-    fv.on('core.form.valid', function() {
-        const $btn = $('#export_submit');
-        const $spinner = $('#loading_spinner');
-
-        // Hiển thị spinner và disable nút
-        $spinner.removeClass('d-none');
-        $btn.prop('disabled', true);
-
-        let id = $('#export_id').val();
-        const formData = new FormData();
-        formData.append('author', $('#export_author').val());
-        formData.append('site', $('#export_site').val());
-        formData.append('type', $('#export_type').val());
-        formData.append('account', $('#accountsExport').val());
-        formData.append('id', id);
-        formData.append('options', JSON.stringify(getRepeaterData()));
-        formData.append('header', $('#export_file_header').val());
-        formData.append('startRow', $('#export_file_start').val());
-        formData.append('sheet_name', $('#export_sheet_name').length ? $('#export_sheet_name').val() : '');
-        formData.append('file', myDropzone.files[0]);
-        formData.append('csrf_token', window.csrfToken);
-
-        $.ajax({
-            url: '../../ajax.php?action=add-xlsx',
-            method: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function (response) {
-                if (response.status === 'inserted' || response.status === 'updated') {
-                    const newId = response.id;
-
-                    // Lấy URL hiện tại và thêm id
-                    const url = new URL(window.location.href);
-                    url.searchParams.set('id', newId);
-
-                    // Reload lại với URL mới
-                    window.location.href = url.toString();
-                } else {
-                    alert('Upload thất bại: ' + response.message);
-                }
-            },
-            error: function (xhr) {
-                console.error('Lỗi:', xhr.responseText);
-                alert('Upload thất bại!');
-            },
-            complete: function () {
-                // Ẩn spinner và bật lại nút
-                $spinner.addClass('d-none');
-                $btn.prop('disabled', false);
-            }
-        });
+function getRepeaterData() {
+    var data = [];
+    // Mỗi row trong repeater có data-repeater-item
+    $('.form-repeater').find('[data-repeater-item]').each(function () {
+        var selectVal = $(this).find('.form-select').val();
+        var inputVal  = $(this).find('input[type="text"]').val();
+        var selectedText = $(this).find('.form-select option:selected').text();
+        if (selectVal !== null && selectVal !== '' && selectVal !== undefined) {
+            data.push({
+                location: selectVal,
+                text :selectedText,
+                value: inputVal
+            });
+        }
     });
+
+    return data;
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    init();
 });
