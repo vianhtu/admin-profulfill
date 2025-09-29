7a48ba4403788ccc3cc0d19c0a47810a6f240e7c
FOX <thienduongvangem.ntt@gmail.com>
2025-09-29 10:29:08 +0700

    ok

--- assets/js/app-xlsx-add.js
+++ assets/js/app-xlsx-add.js
@@ -288,7 +288,6 @@ function getRepeaterData() {
 }
 
 function addXlsxFile(fv, dz) {
-    console.log(fv.getFields());
     // Khi FormValidation validate thành công
     fv.on('core.form.valid', function() {
         const $btn = $('#export_submit');
