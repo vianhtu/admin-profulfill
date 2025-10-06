92b99eb68bd419b7409844485f231c5b05a8a7d1
FOX <thienduongvangem.ntt@gmail.com>
2025-10-06 16:08:56 +0700

    ok

--- assets/js/app-teams-list.js
+++ assets/js/app-teams-list.js
@@ -121,6 +121,9 @@ function initTable(){
                     render: (data, type, full, meta) => {
                         return `
                       <div class="d-flex align-items-center">
+                        <a href="index.php?menu=users&UserTeam=${full['id']}" class="btn btn-text-secondary rounded-pill waves-effect btn-icon">
+                          <i class="icon-base ti tabler-eye icon-22px"></i>
+                        </a>
                         <a href="javascript:;" class="btn btn-text-secondary rounded-pill waves-effect btn-icon delete-record">
                           <i class="icon-base ti tabler-trash icon-22px"></i>
                         </a>
