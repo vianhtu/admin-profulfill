68d2dd683bf89fe214f3b0c60db89469ad719c51
FOX <thienduongvangem.ntt@gmail.com>
2025-10-12 16:10:39 +0700

    ok

--- functions.php
+++ functions.php
@@ -588,10 +588,11 @@ function actionDownloadTableGemini(): array
     $ids = [];
     while ($row = $result->fetch_assoc()) {
         $downloadId = $row['ID'];
-        $ids[$row['ID']] = AIProcessDownloadProducts($downloadId);
+        //$ids[$row['ID']] = AIProcessDownloadProducts($downloadId);
     }
 
-    return ['status' => 'success', 'ids' => $ids];
+    //return ['status' => 'success', 'ids' => $ids];
+    return ['status' => 'success', 'ids' => [197=>['status' => 'success', 'name' => '197']]];
 }
 
 function getDataTableParams(array $allowedCols, string $defaultCol = 'ID'): array {
