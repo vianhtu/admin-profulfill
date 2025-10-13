92b4acc9020e21814e03d50cffe2e4d29d89267a
FOX <thienduongvangem.ntt@gmail.com>
2025-10-13 15:09:34 +0700

    ok

--- model/functions-openai.php
+++ model/functions-openai.php
@@ -74,15 +74,7 @@ function openai_create_and_upload_batch_file(string $jsonl_content): array
                     'name'     => 'file',
                     'contents' => fopen($file_path, 'rb'),
                     'filename' => $file_name,
-                ],
-                [
-                    'name'     => 'expires_after[anchor]',
-                    'contents' => 'last_active_at',
-                ],
-                [
-                    'name'     => 'expires_after[days]',
-                    'contents' => '2',
-                ],
+                ]
             ],
             'http_errors' => false,
         ]);
