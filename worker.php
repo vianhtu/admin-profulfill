9caad476302c900bdcf8529ef2f65db9568c49b3
FOX <thienduongvangem.ntt@gmail.com>
2025-10-14 16:15:00 +0700

    ok

--- worker.php
+++ worker.php
@@ -95,11 +95,11 @@ $downloadId = (int)$argv[1] ?? 0;
 $batch_name = $argv[2] ?? '';
 $ai_name    = $argv[3] ?? '';
 $team_id    = (int)$argv[4] ?? 0;
-writeLog($team_id);
-exit();
+
 if ($downloadId == 0 || $batch_name == '' || $ai_name == '' || $team_id == 0) {
     exit();
 }
+
 $_SESSION['auth'] = ['user_id' => 0, 'team' => $team_id];
 switch ($ai_name) {
     case 'google':
