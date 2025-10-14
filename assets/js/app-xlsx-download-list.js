4432d79a191afd62e0d65f79568601233f441dae
FOX <thienduongvangem.ntt@gmail.com>
2025-10-14 11:26:19 +0700

    ok

--- assets/js/app-xlsx-download-list.js
+++ assets/js/app-xlsx-download-list.js
@@ -409,6 +409,13 @@ function initTable(){
                                             action: function (e, dt, node, config) {
                                                 actionModel(dt,'gpt-5-nano-2025-08-07', 'openai');
                                             }
+                                        },
+                                        {
+                                            text: '<span class="d-flex align-items-center"><i class="icon-base ti tabler-brand-openai me-1"></i>OpenAI GPT 5 Mini</span>',
+                                            className: 'dropdown-item',
+                                            action: function (e, dt, node, config) {
+                                                actionModel(dt,'gpt-5-mini-2025-08-07', 'openai');
+                                            }
                                         }
                                     ]
                                 },
