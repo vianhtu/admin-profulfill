63afe9708454b5172c3c162d2d9591fb62778048
FOX <thienduongvangem.ntt@gmail.com>
2025-10-14 15:44:31 +0700

    ok

--- functions.php
+++ functions.php
@@ -2790,7 +2790,7 @@ function formatCurrencyVND($input): string
 function getDebug()
 {
     $_SESSION['auth'] = ['user_id' => 1, 'team' => 1];
-    return openai_get_batches_by_name('batch_68ed14538c0481909b884178b3757591');
+    return openai_get_batches_by_name('batch_68edd78c58908190a7cdf5777118cc1a');
     // Spawn worker
     //$cmd = "php " . __DIR__ . "/worker.php 173 batches/zwddup4vdx5tpo5srm92tz6rvgvah3rxqifu > /dev/null 2>&1 &";
     //exec($cmd);
