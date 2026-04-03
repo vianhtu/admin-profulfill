d2650084d4ad22e2d64478809e8693462ee69c84
FOX <thienduongvangem.ntt@gmail.com>
2026-04-03 15:10:29 +0700

    Update app-store-list.php

--- html/vertical-menu-template-no-customizer/app-store-list.php
+++ html/vertical-menu-template-no-customizer/app-store-list.php
@@ -8,8 +8,8 @@ if(!checkRoles('view', 'exports_xlsx')){
     <div class="card-header border-bottom">
         <h5 class="card-title mb-0">Filters</h5>
         <div class="d-flex justify-content-between align-items-center row pt-4 gap-4 gap-md-0">
-            <div class="col-md-4 filter-teams"></div>
             <div class="col-md-4 filter-sites"></div>
+            <div class="col-md-4 filter-teams"></div>
             <div class="col-md-4 filter-authors"></div>
         </div>
     </div>
