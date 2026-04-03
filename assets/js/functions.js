01f899b481bd594ae21f818fa71da1255dbaa4ef
FOX <thienduongvangem.ntt@gmail.com>
2026-04-03 16:11:03 +0700

    ok

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -185,7 +185,6 @@ function getSelect2filterTable(api, id, html_class, col, label, options = {}, se
 async function fetchTableFilter(action = 'get-products-table-filter', data = {}){
     const res = await fetch('../../ajax.php?action='+ action, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
         data: data
     });
     if (!res.ok) throw new Error('Lỗi lấy danh mục');
