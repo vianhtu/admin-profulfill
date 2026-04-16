4bb3e8689172fa3980d5733ee94c810434883909
FOX <thienduongvangem.ntt@gmail.com>
2026-04-16 14:29:06 +0700

    ok

--- config.php
+++ config.php
@@ -159,7 +159,7 @@ function get_user_by_id(int $id): ?array {
             // Ép kiểu dữ liệu để đảm bảo tính nhất quán
             $user['id'] = (int)$user['id'];
             $user['team'] = (int)$user['team'];
-
+            var_dump($user); exit();
             return $user;
         }
     } catch (mysqli_sql_exception $e) {
