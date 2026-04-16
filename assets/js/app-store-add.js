2ef606583049a7b3cd508ecaea5fd62483f810d3
FOX <thienduongvangem.ntt@gmail.com>
2026-04-16 08:49:00 +0700

    Update app-store-add.js

--- assets/js/app-store-add.js
+++ assets/js/app-store-add.js
@@ -27,7 +27,7 @@ function init(){
         });
     }
 
-    ajaxSelect2('link_accounts', 'filter-accounts', false);
+    ajaxSelect2('link_accounts', 'filter-accounts', true);
 }
 
 function repeaterOptions() {
