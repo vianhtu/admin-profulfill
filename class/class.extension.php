663d51ee3ebf3a06f621f2ad3f0d58382160fe9c
FOX <thienduongvangem.ntt@gmail.com>
2026-05-22 15:14:22 +0700

    Update class.extension.php

--- class/class.extension.php
+++ class/class.extension.php
@@ -80,7 +80,7 @@ class Extensions
         $conn = db();
 
         // 1. Chuẩn bị câu lệnh SQL (Chỉ lấy ID và status)
-        $sql = "SELECT ID, host_id, status FROM orders WHERE account_id = ? AND status IN ('Unshipped', 'Shipped')";
+        $sql = "SELECT ID, host_id, status FROM orders WHERE account_id = ? AND status IN ('unshipped', 'shipped')";
 
         try {
             $result = $conn->execute_query($sql, [$account_id]);
