3dbf74454472d0cd5c367ffefc529d26ecbf2cb0
FOX <thienduongvangem.ntt@gmail.com>
2026-06-16 17:00:17 +0700

    Update app-ecommerce-order-list.php

--- html/vertical-menu-template-no-customizer/app-ecommerce-order-list.php
+++ html/vertical-menu-template-no-customizer/app-ecommerce-order-list.php
@@ -105,7 +105,7 @@ if(!checkRoles('view', 'orders')){
                             <input type="hidden" name="item_id" id="item_id" value="">
                             <input type="hidden" name="item_name" id="order_id" value="">
                             <div class="mb-3">
-                                <label for="item-base-cost" class="form-label font-weight-bold">Base Cost</label>
+                                <label for="item-base-cost" class="form-label font-weight-bold">Base Cost $</label>
                                 <input type="number" step="0.01" class="form-control" id="item-base-cost" placeholder="9.99">
                             </div>
                             <div class="mb-3">
