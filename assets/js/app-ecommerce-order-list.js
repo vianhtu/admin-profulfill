101da099098f4c5a74054ed1dfe17e943c160631
FOX <thienduongvangem.ntt@gmail.com>
2026-06-17 09:59:43 +0700

    Update app-ecommerce-order-list.js

--- assets/js/app-ecommerce-order-list.js
+++ assets/js/app-ecommerce-order-list.js
@@ -847,17 +847,17 @@ function renderShipCountdownHtml(shipDateStr, status) {
         const hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const hh = hours.toString().padStart(2, '0');
         textClass = "text-danger";
-        countdownText = `<i class="icon-base ti tabler-alert-circle me-1"></i> ${hh}:${mm}:${ss}`;
+        countdownText = `<i class="icon-base ti tabler-alert-circle me-1"></i>${hh}:${mm}:${ss}`;
     } else if (days < 2) {
         // Còn từ 1 đến dưới 2 ngày -> Tính tổng số giờ (Ví dụ: từ 24h đến 47h)
         const totalHours = Math.floor(diffTime / (1000 * 60 * 60));
         const hh = totalHours.toString().padStart(2, '0');
         textClass = "text-warning";
-        countdownText = `<i class="icon-base ti tabler-alert-triangle me-1"></i> ${hh}:${mm}:${ss}`; // Hiển thị tổng số giờ
+        countdownText = `<i class="icon-base ti tabler-alert-triangle me-1"></i>${hh}:${mm}:${ss}`; // Hiển thị tổng số giờ
     } else {
         // Còn trên 2 ngày -> Hiện số ngày thong thả
         textClass = "text-success";
-        countdownText = `<i class="icon-base ti tabler-clock me-1"></i> ${days} days left`;
+        countdownText = `<i class="icon-base ti tabler-clock me-1"></i>${days} days left`;
     }
 
     return `<span class="${textClass} text-nowrap d-inline-flex align-items-center">${countdownText}</span>`;
