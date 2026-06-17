efe4815db3dddc49a388476adbaea33c27b92e06
FOX <thienduongvangem.ntt@gmail.com>
2026-06-17 09:12:01 +0700

    Update app-ecommerce-order-list.js

--- assets/js/app-ecommerce-order-list.js
+++ assets/js/app-ecommerce-order-list.js
@@ -840,7 +840,7 @@ function renderShipCountdownHtml(shipDateStr, status) {
         const hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const hh = hours.toString().padStart(2, '0');
         textClass = "text-danger fw-bold";
-        countdownText = `!${hh}:${mm}:${ss}`;
+        countdownText = `<i class="icon-base ti tabler-basket-exclamation"></i> ${hh}:${mm}:${ss}`;
     } else if (days < 2) {
         // Còn từ 1 đến dưới 2 ngày -> Tính tổng số giờ (Ví dụ: từ 24h đến 47h)
         const totalHours = Math.floor(diffTime / (1000 * 60 * 60));
