717eb30b33dc4015de0fc348be2172447119487c
FOX <thienduongvangem.ntt@gmail.com>
2026-06-18 09:08:42 +0700

    Update app-store-list.js

--- assets/js/app-store-list.js
+++ assets/js/app-store-list.js
@@ -182,7 +182,7 @@ function initTable(){
                     targets: 3,
                     render: function (data, type, full, meta) {
                         return '<div class="d-flex flex-column">' +
-                            '<span class="fw-medium">'+ teamsObj[full['team_id']].title +'</span>' +
+                            '<span class="fw-medium">'+ teamsObj?.[full['team_id']]?.title +'</span>' +
                             //'<small>'+ authorsObj[full['author_id']].title +'</small>' +
                             '</div>';
                     }
